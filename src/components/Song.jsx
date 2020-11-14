e741633df1c1da04f3316b29ee95810e2ac7711f
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-11-14 18:49:19 +0630

    Added initial styles for song and player

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Song = () => {
   return (
     <div className="song-container">
-      <h1>song</h1>
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
     </div>
   );
 };
